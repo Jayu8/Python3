c13ae8745362b0b196b406cfade9991389950078
Jayu8 <narrajayakrishna@outlook.com>
2017-06-21 12:15:10 -0400

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -37,15 +37,18 @@ me = Classy()
 print me.getClassiness()
 
 me.addItem("tophat")
+
 # Should be 2
 print me.getClassiness()
 
 me.addItem("bowtie")
 me.addItem("jacket")
 me.addItem("monocle")
+
 # Should be 11
 print me.getClassiness()
 
 me.addItem("bowtie")
+
 # Should be 15
 print me.getClassiness()
