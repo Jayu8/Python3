f17a040e9652f8e59a94a22518e419c6a3c8c7cd
Jayu8 <narrajayakrishna@outlook.com>
2017-06-27 19:45:15 -0400

    Update Iterator_Iterables.py

--- Iterator_Iterables.py
+++ Iterator_Iterables.py
@@ -1,10 +1,24 @@
-#Basic
-for element in [1, 2, 3]:
-    print(element)
+""""
+An iterable is a object which has a __iter__() method.
+It can possibly iterated over several times, such as list()s and tuple()s.
+It returns an iterator object
+An iterator is the object which iterates. 
+It is returned by an __iter__() method, returns itself via its own __iter__() method and has a __next()__ method.
+Iteration is the process of calling this __next__() until it raises StopIteration
+"""
+
+
 """
 Behind the scenes, the for statement calls iter() on the container object. 
 The function returns an iterator object that defines the method __next__() which accesses elements in the container one at a time. 
 When there are no more elements, __next__() raises a StopIteration exception which tells the for loop to terminate. 
+"""
+for element in [1, 2, 3]:
+    print(element)
+
+    
+"""
+A more spread out version
 """
 s = 'cat'      # s is an ITERABLE
                    # s is a str object that is immutable
@@ -27,10 +41,3 @@ Traceback (most recent call last):
 StopIteration
 
 iter(t) is t   # the iterator is self-iterable
-""""
-An iterable is a object which has a __iter__() method.
-It can possibly iterated over several times, such as list()s and tuple()s.It returns an iterator object
-An iterator is the object which iterates. 
-It is returned by an __iter__() method, returns itself via its own __iter__() method and has a __next()__ method.
-Iteration is the process of calling this __next__() until it raises StopIteration
-"""
